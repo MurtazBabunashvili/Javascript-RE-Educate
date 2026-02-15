8c7c3c7ebd3ec6446a63b8f419e8e801b9e710d6
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2026-02-15 15:11:52 +0400

    feat(update): fixed typo

--- Homework 37 - Backend/src/gateway/gateway.socket.ts
+++ Homework 37 - Backend/src/gateway/gateway.socket.ts
@@ -1,23 +1,29 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
-    corst: '*'
+  cors: '*',
 })
 export class GatewaySocket {
-    @WebSocketServer() server: Server
+  @WebSocketServer() server: Server;
 
-    @SubscribeMessage('joinRoom')
-    handleJoin(
-        @MessageBody() data: {roomId:any; email: any},
-        @ConnectedSocket() client: Socket
-    ){
-        console.log(`${data.email} joined ${data.roomId}`)
-        client.join(data.roomId)
-    }
+  @SubscribeMessage('joinRoom')
+  handleJoin(
+    @MessageBody() data: { roomId: any; email: any },
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log(`${data.email} joined ${data.roomId}`);
+    client.join(data.roomId);
+  }
 
-    @SubscribeMessage('onPrivateMessage')
-    handleMessage(@MessageBody() body: any) {
-        this.server.to(body.roomId).emit('privateMessage', body)
-    }
+  @SubscribeMessage('onPrivateMessage')
+  handleMessage(@MessageBody() body: any) {
+    this.server.to(body.roomId).emit('privateMessage', body);
+  }
 }
